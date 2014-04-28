ad4a18093a8014de786020cca71b5d13a3ac35c7
Mark L. Chang <mark.chang@gmail.com>
2014-04-28 00:37:08 -0700

    responding using client

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,31 @@ router.get('/', function(req, res) {
 });
 
 router.post('/sms', function(req, res) {
+  // deets
   console.log("SMS received");
-  console.log('From ' + req.param('From'));
-  console.log('MessageSID ' + req.param('MessageSid'));
-  console.log('Body ' + req.param('Body'));
 
-  var resp = new twilio.TwimlResponse();
-  resp.say("Hi there");
-  res.send(resp.toString());
+  var from = req.param('From');
+  var message_sid = req.param('MessageSid');
+  var body = req.param('Body');
+
+  console.log("From: " + from + ", body: " + body);
+
+  client.sendSms({
+      to: from,
+      from:'12065382935',
+      body:'Got it'
+  }, function(error, message) {
+      if (!error) {
+          console.log('Success! The SID for this SMS message is:');
+          console.log(message.sid);
+          console.log('Message sent on:');
+          console.log(message.dateCreated);
+      } else {
+          console.log('Oops! There was an error sending a response.');
+      }
+  });
+
+  res.end();
 })
 
 module.exports = router;
