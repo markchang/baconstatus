2064d86d2a37403e9369d9bb88aa59d1d60b66e7
Mark L. Chang <mark.chang@gmail.com>
2014-04-28 00:02:53 -0700

    twilio handling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var twilio = require('twilio');
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/sms', function(req, res) {
+  console.log("SMS received");
+
+  var resp = new twilio.TwimlResponse();
+  resp.say('ok there');
+  console.log(resp.toString());
+  res.send("ok");
+})
 
 module.exports = router;
