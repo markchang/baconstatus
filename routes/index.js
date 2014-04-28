badad390b032e8a3c51c46772198ef9226cafb04
Mark L. Chang <mark.chang@gmail.com>
2014-04-28 00:07:04 -0700

    post

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/sms', function(req, res) {
+router.post('/sms', function(req, res) {
   console.log("SMS received");
+  console.log(req.toString());
 
   var resp = new twilio.TwimlResponse();
-  resp.say('ok there');
+  resp.say('got it');
   console.log(resp.toString());
-  res.send("ok");
+  res.send(resp.toString());
 })
 
 module.exports = router;
